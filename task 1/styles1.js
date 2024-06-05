0a927838aac623cce422c8d547bacca1a6e7e357
srivallitaninki <srivallitaninki@gmail.com>
2024-06-05 20:29:56 +0530

    Add files via upload

---
+++ task 1/styles1.js
@@ -0,0 +1,9 @@
+/* script.js */
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = '#222';
+    } else {
+        navbar.style.backgroundColor = '#333';
+    }
+});
